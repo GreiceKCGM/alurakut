4b46282d96b290eacf9ee04a7a07960355639641
Greice Kelli <greicekellybc@gmail.com>
2021-07-12 15:48:17 -0300

    primeiros componentes e estilos do AluraKut

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+
+  *{
+    margin:0;
+    padding:0;
     box-sizing: border-box;
+  }
+  body {
+   
+    font-family:sans-serif;
+    background-color: #D9E6F6;
+  }
+  
+  #__next {
+    display:flex;
+    min-height: 100vh;
+    flex-direction:column;
   }
 `
 
