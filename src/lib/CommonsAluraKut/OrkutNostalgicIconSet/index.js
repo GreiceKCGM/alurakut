cde7e388e91ddf172dff59b2a0212adc8aaa090f
Greice Kelli <greicekellybc@gmail.com>
2021-07-13 09:19:18 -0300

    instalação do eslint e algumas correções

--- src/lib/CommonsAluraKut/OrkutNostalgicIconSet/index.js
+++ src/lib/CommonsAluraKut/OrkutNostalgicIconSet/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import OrkutNostalgicIconSetList from'./styled/styled';
+import OrkutNostalgicIconSetList from './styled/styled';
 
-// ================================================================================================================
+// ===================================================================
 // OrkutNostalgicIconSet
-// ================================================================================================================
-export  default function OrkutNostalgicIconSet(props) {
+// ==========================================================================
+export default function OrkutNostalgicIconSet(props) {
   return (
     <OrkutNostalgicIconSetList>
       {[
@@ -19,7 +19,11 @@ export  default function OrkutNostalgicIconSet(props) {
             {name}
           </span>
           <span className="OrkutNostalgicIconSet__number" style={{ gridArea: 'number' }}>
-            <img key={`orkut__icon_set__${slug}_img`} className="OrkutNostalgicIconSet__iconSample" src={`https://alurakut.vercel.app/icons/${icon}.svg`} />
+            <img
+              key={`orkut__icon_set__${slug}_img`}
+              className="OrkutNostalgicIconSet__iconSample"
+              src={`https://alurakut.vercel.app/icons/${icon}.svg`}
+            />
             {props[slug] ? props[slug] : 0}
           </span>
         </li>
@@ -38,15 +42,18 @@ export  default function OrkutNostalgicIconSet(props) {
             <span className="OrkutNostalgicIconSet__iconComplex" className="OrkutNostalgicIconSet__number" style={{ gridArea: 'number' }}>
               {[0, 1, 2].map((_, index) => {
                 const isHeartActive = index <= (total - 1);
-                return <img key={`orkut__icon_set__${slug}_img_${index}`} src={`https://alurakut.vercel.app/icons/${icon}.svg`} style={{ marginRight: '2px', opacity: isHeartActive ? 1 : '0.5' }} />
+                return (
+                  <img
+                    key={`orkut__icon_set__${slug}_img_${index}`}
+                    src={`https://alurakut.vercel.app/icons/${icon}.svg`}
+                    style={{ marginRight: '2px', opacity: isHeartActive ? 1 : '0.5' }}
+                  />
+                );
               })}
             </span>
           </li>
         );
       })}
     </OrkutNostalgicIconSetList>
-  )
+  );
 }
-
-
-
